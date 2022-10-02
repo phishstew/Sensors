868eea8b9af0c1bbbe7a12af031b5227f8174c92
phishstew <vkunnummal1@gmail.com>
2022-10-02 15:24:06 -0400

    Initial Commit

---
+++ Sensor Output/Sensor_OutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sensor_OutputApp.swift
+//  Sensor Output
+//
+//  Created by Vishnu Kunnummal on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Sensor_OutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
